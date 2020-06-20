4c3534cb2b7d7766f15bdd458b0f15dd3411e042
RamakriG <gopinath.ramakrishnappa@hp.com>
2020-06-20 16:31:56 +0530

    updated second script

--- cypress/integration/examples/AutoTestScript.spec.js
+++ cypress/integration/examples/AutoTestScript.spec.js
@@ -17,6 +17,21 @@ describe('suitename - cypress hooks', () => {
         cy.get('input[value="Log in"]').click();
 
     })
+
+
+
+    it('visit App - Second test script', () => {
+
+      cy.visit('http://eaapp.somee.com/');
+      cy.get('a').contains('Login').click();
+      cy.get('label').contains('Password').should('be.visible');
+      cy.get('input[name="UserName"]').should('be.visible');
+      cy.get('input[name="UserName"]').type('admin');
+      cy.get('input[name="Password"]').type('password');
+      cy.get('input[value="Log in"]').should('be.visible')
+      cy.get('input[value="Log in"]').click();
+
+  })
 })
 
 
