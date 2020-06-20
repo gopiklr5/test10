3b92ce0acf52b76c173fc2f3b7a870539c215b86
RamakriG <gopinath.ramakrishnappa@hp.com>
2020-06-20 15:58:16 +0530

    New script to test dashboard

---
+++ cypress/integration/examples/AutoTestScript.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+
+describe('suitename - cypress hooks', () => {
+
+
+
+
+    it('visit App - first test script', () => {
+
+        cy.visit('http://eaapp.somee.com/');
+      //  cy.get('a').contains('Login').click();
+        cy.get('label').contains('Password').should('be.visible');
+        cy.get('input[name="UserName"]').type('admin');
+        cy.get('input[name="Password"]').type('password');
+        cy.get('input[value="Log in"]').click();
+
+    })
+})
+
+
+
+
