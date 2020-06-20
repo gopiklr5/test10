ddbb7702af885b24f59ac26e0b1d71c516d4bb0e
RamakriG <gopinath.ramakrishnappa@hp.com>
2020-06-20 16:02:26 +0530

    New changes

--- cypress/integration/examples/AutoTestScript.spec.js
+++ cypress/integration/examples/AutoTestScript.spec.js
@@ -11,6 +11,7 @@ describe('suitename - cypress hooks', () => {
         cy.visit('http://eaapp.somee.com/');
       //  cy.get('a').contains('Login').click();
         cy.get('label').contains('Password').should('be.visible');
+        cy.get('input[name="UserName"]').type('admin').should('be.visible');
         cy.get('input[name="UserName"]').type('admin');
         cy.get('input[name="Password"]').type('password');
         cy.get('input[value="Log in"]').click();
